b74d80fc5bd36033b13f925e9c9989b04f2995dd
PaulineDuarte <Paulineduarte1@hotmail.com>
2024-11-26 13:18:25 -0300

    Update get by id

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
-    headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+    baseURL: 'https://szgyytinxbrwphdqtjrg.supabase.co/rest/v1',
+    headers:{
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6Z3l5dGlueGJyd3BoZHF0anJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI2MzIyMTcsImV4cCI6MjA0ODIwODIxN30.tMmmmJfxsVHMIn9E5SD8_RKHrSNZWzLb-WleUBzA-Ic",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6Z3l5dGlueGJyd3BoZHF0anJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI2MzIyMTcsImV4cCI6MjA0ODIwODIxN30.tMmmmJfxsVHMIn9E5SD8_RKHrSNZWzLb-WleUBzA-Ic"
     }
 })
